397deecdb3398c85ac58c61ed964029f21909158
shmesa22 <shmesa23@gmail.com>
2017-07-05 21:44:07 -0500

    Solve kata: Even or Odd

---
+++ 002-even-or-odd.js
@@ -0,0 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd"
+}
