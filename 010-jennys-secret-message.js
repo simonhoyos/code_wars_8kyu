24bdae3065380992d322bb1f2c7c70623f97e4fc
shmesa22 <shmesa23@gmail.com>
2017-07-06 18:08:02 -0500

    Solve kata: Jenny's secret message

---
+++ 010-jennys-secret-message.js
@@ -0,0 +1,9 @@
+// Jenny has written a function that returns a greeting for a user. However,
+// she's in love with Johnny, and would like to greet him slightly different.
+// She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+function greet(name){
+  return name !== "Johnny" ? "Hello, " + name + "!" : "Hello, my love!";
+}
