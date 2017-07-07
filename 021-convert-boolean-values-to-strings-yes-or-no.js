6143047e530908660e8f20b3d9a1ec66fe277bba
shmesa22 <shmesa23@gmail.com>
2017-07-07 18:27:19 -0500

    Solve kata: Convert boolean values to strings 'Yes' or 'No'.

---
+++ 021-convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,9 @@
+// Complete the bool_to_word (Javascript: boolToWord ) method.
+
+// Given: a boolean value
+
+// Return: a 'Yes' string for true and a 'No' string for false
+
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No';
+}
