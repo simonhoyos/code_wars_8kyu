e46b7dc90bb02cda36a57f2378187754191e85ac
shmesa22 <shmesa23@gmail.com>
2017-07-08 23:24:45 -0500

    Solve kata: Exclamation marks series #1: Remove an exclamation mark from the end of string

---
+++ 033-exclamation-mark-series-1-remove-a-exclamation-mark-from-the-end-of-the-string.js
@@ -0,0 +1,6 @@
+// Remove a exclamation mark from the end of string. For a beginner kata, you
+// can assume that the input data is always a string, no need to verify it.
+
+function remove(s){
+  return s.replace(/\!$/, '');
+}
