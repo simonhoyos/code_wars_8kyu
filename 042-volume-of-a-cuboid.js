116e7bdc1b7db33584820e49bf1834f8ed5a4c7f
shmesa22 <shmesa23@gmail.com>
2017-07-09 19:36:25 -0500

    Solve kata: Volume of a Cuboid

---
+++ 042-volume-of-a-cuboid.js
@@ -0,0 +1,16 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values:
+// length, width and the height of the cuboid. Write a function to help Bob
+// with this calculation.
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
