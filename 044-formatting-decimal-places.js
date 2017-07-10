e966b951320e30bf40b82e11d7951a9daf083c0a
shmesa22 <shmesa23@gmail.com>
2017-07-09 20:03:55 -0500

    Solve kata: Formatting decimal places #0

---
+++ 044-formatting-decimal-places.js
@@ -0,0 +1,7 @@
+// Each number should be formatted that it is rounded to two decimal places.
+// You don't need to check whether the input is a valid number because only
+// valid numbers are used in the tests.
+
+function twoDecimalPlaces(n) {
+  return Number(n.toFixed(2));
+}
