e550d36e4c88d09e27dda02828bfcfbdb6716bfa
shmesa22 <shmesa23@gmail.com>
2017-07-09 19:43:53 -0500

    Solve kata: DNA to RNA Conversion

---
+++ 043-dna-to-rna-conversion.js
@@ -0,0 +1,13 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in
+// biological systems. It is composed of four nucleic acid bases Guanine
+// ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA
+// differs slightly from DNA its chemical structure and contains no Thymine.
+// In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a funciton which translates a given DNA string into RNA.
+
+function DNAtoRNA(dna) {
+  return dna.replace(/t/ig, "U"); 
+}
