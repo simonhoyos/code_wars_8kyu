95416710632d9dc7db47e7a6cd4e414357f17a29
shmesa22 <shmesa23@gmail.com>
2017-07-13 12:53:13 -0500

    Solve kata: Simple multiplication

---
+++ 075-simple-multiplication.js
@@ -0,0 +1,6 @@
+// This kata is about multiplying a given number by eight if it is an even
+// number and by nine otherwise.
+
+function simpleMultiplication(n){
+  return n % 2 === 0 ? n * 8 : n * 9;
+}
