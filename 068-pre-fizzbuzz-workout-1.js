ed71d87a608d338d65508ddca700a48ff22b3b6e
shmesa22 <shmesa23@gmail.com>
2017-07-12 23:08:32 -0500

    Solve kata: Pre-FizzBuzz Workout #1

---
+++ 068-pre-fizzbuzz-workout-1.js
@@ -0,0 +1,17 @@
+// This is the first step to understanding FizzBuzz.
+
+// Your inputs: a positive integer, n, greater than or equal to one. n is
+// provided, you have NO CONTROL over its value.
+
+// Your expected outputs: an array of positive integers from 1 to n
+
+// Your job is to write an algorithm that gets you from the input to the
+// output.
+
+function preFizz(n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    array.push(i);
+  }
+  return array;
+}
