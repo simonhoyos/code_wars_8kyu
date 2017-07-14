6f992b5053ba3b990cbf14156316737789aaf1d5
shmesa22 <shmesa23@gmail.com>
2017-07-14 11:25:42 -0500

    Solve kata: Grasshopper - Personalized Message

---
+++ 082-grasshopper-personalized-message.js
@@ -0,0 +1,11 @@
+// Personalized greeting
+
+// Create a function that gives a personalized greeting. This function takes
+// two parameters: name and owner.
+
+// Use conditionals to return the proper message: case | return --- | --- name
+// equals owner | 'Hello boss' otherwise | 'Hello guest'
+
+function greet (name, owner) {
+  return `Hello ${name === owner ? "boss" : "guest"}`;
+}
