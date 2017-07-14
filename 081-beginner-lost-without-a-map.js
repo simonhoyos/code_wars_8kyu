2974381905aa4b6b23f456bffa911ebf284fca1d
shmesa22 <shmesa23@gmail.com>
2017-07-14 11:21:04 -0500

    Solve kata: Beginner - Lost Without a Map

---
+++ 081-beginner-lost-without-a-map.js
@@ -0,0 +1,11 @@
+// Given and array of integers (x), return the array with each value doubled.
+// Example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// For the beginner, try to use the map method - it comes in very handy quite
+// a lot so is a good one to know.
+
+function maps(x){
+  return x.map((n) => n * 2);
+}
